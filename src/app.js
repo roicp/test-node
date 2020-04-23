c0af8dedb3a14b447a2f7f2824e99b6fa5a6c536
Rodrigo Ipaves de Campos Pinto <ipaves@gmail.com>
2020-04-23 14:17:33 -0300

    config lint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-router.get('/', (req, res, next) => {
-   return res.status(200).json({ message: 'Welcome to Express API template' });
-});
+const router = express.Router();
+
+router.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
 
 module.exports = router;
